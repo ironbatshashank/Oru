1c8819d2347ad8dbfc132801bdb491fa0be64872
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-10 15:28:30 +0530

    Registration ui

--- app/src/main/java/com/example/shashank/oru/RegistrationActivity.java
+++ app/src/main/java/com/example/shashank/oru/RegistrationActivity.java
@@ -2,12 +2,16 @@ package com.example.shashank.oru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private Button mRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        
     }
 }
