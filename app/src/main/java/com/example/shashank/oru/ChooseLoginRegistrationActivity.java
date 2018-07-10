4a11a8d8b42ed4e56cd574685dc3cabd12c92cb9
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-10 15:11:46 +0530

    Added login

--- app/src/main/java/com/example/shashank/oru/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/example/shashank/oru/ChooseLoginRegistrationActivity.java
@@ -1,13 +1,38 @@
 package com.example.shashank.oru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
+    private Button mLogin, mRegister ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registration);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
