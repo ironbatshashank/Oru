186eb4fe2a74bebd40bb741262e09fe08f864a83
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-12 12:34:37 +0530

    Fixed bugs

--- app/src/main/java/com/example/shashank/oru/MainActivity.java
+++ app/src/main/java/com/example/shashank/oru/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         checkUserSex();
 
         al = new ArrayList<>();
