5243a745dec07a08984434f596f5bf4965981605
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-14 18:36:10 +0530

    adapter

--- app/src/main/java/com/example/shashank/oru/arrayAdapter.java
+++ app/src/main/java/com/example/shashank/oru/arrayAdapter.java
@@ -1,8 +1,29 @@
 package com.example.shashank.oru;
 
+import android.animation.LayoutTransition;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 /**
  * Created by shashank on 7/14/2018.
  */
 
-public class arrayAdapter {
+public class arrayAdapter extends ArrayAdapter<cards>{
+
+    Context context;
+
+    public arrayAdapter(Context context, int resourceId, List<cards> items){
+        super(context, resourceId, items)
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        cards card_item = getItem(position);
+
+        convertView = LayoutInflater(getViewTypeCount()).inflate(R.layout.item, parent false);
+    }
 }
