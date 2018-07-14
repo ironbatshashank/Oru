d1768f08238a8359bb456b7d4e21778272ce7de8
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-14 19:05:48 +0530

    Completed arrayadapter

--- app/src/main/java/com/example/shashank/oru/arrayAdapter.java
+++ app/src/main/java/com/example/shashank/oru/arrayAdapter.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -27,7 +29,14 @@ public class arrayAdapter extends ArrayAdapter<cards>{
         if(convertView == null) {
 
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent false);
-        
         }
+
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        ImageView image = (ImageView) convertView.findViewById(R.id.image);
+
+        name.setText(card_item.getName());
+        image.setImageResource(R.mipmap.ic_launcher);
+
+        return convertView;
     }
 }
