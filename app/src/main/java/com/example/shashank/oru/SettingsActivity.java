fde0e30a9df6284ebdb180cfd62d273af18f4d77
Shashank Srivastava <shashanksri65@gmail.com>
2018-07-16 08:44:48 +0530

    Fixed database reference

--- app/src/main/java/com/example/shashank/oru/SettingsActivity.java
+++ app/src/main/java/com/example/shashank/oru/SettingsActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -51,6 +52,8 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        String userSex = getIntent().getExtras().getString("userSex");
+
         mNameField = (EditText) findViewById(R.id.name);
 
         mPhoneField = (EditText) findViewById(R.id.phone);
@@ -62,7 +65,7 @@ public class SettingsActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         userId = mAuth.getCurrentUser().getUid();
-        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userId);
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userSex).child(userId);
 
         getUserInfo();
         mProfileImage.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +81,13 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 saveUserInformation();
+            }
+        });
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                return;
             }
         });
     }
@@ -97,6 +107,7 @@ public class SettingsActivity extends AppCompatActivity {
                     }
                     if(map.get("profileImageUrl")!=null){
                         mPhoneField.setText(phone);
+                        Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
                     }
                 }
             }
@@ -140,8 +151,9 @@ public class SettingsActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
                     Map userInfo = new HashMap();
-                    userInfo.put("profileImageUrl", downloadUrl);
+                    userInfo.put("profileImageUrl", downloadUrl.toString());
                     mCustomerDatabase.updateChildren(userInfo);
 
                     finish();
